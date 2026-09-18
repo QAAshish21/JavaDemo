bac157c7b25277f568990ab63aa8b21f4793d488
Ashishsawarkar98 <ashishsawarkar95@gmail.com>
2026-09-18 13:29:28 +0530

    Added Condition statement demo

---
+++ src/day4/MultipleStatementDemo.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class MultipleStatementDemo {
+
+	public static void main(String[] args) {
+
+		if (false) {
+			System.out.println("Excute If block beacuse condition is true");
+		} else {
+			System.out.println("Execute else bloack beacuse conditon is false ");
+		}
+	}
+}
