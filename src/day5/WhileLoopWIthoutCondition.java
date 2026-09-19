f2dea56e63271ee2500efb551c236e891e42bc16
Ashishsawarkar98 <ashishsawarkar95@gmail.com>
2026-09-19 16:26:17 +0530

    Added day 5 looping statement

---
+++ src/day5/WhileLoopWIthoutCondition.java
@@ -0,0 +1,23 @@
+package day5;
+
+public class WhileLoopWIthoutCondition {
+
+	public static void main(String[] args) {
+		
+		
+		int i=1;
+		
+		while(true)
+		{
+			System.out.println("QA");
+			i++;
+			
+			if(i==10)
+			{
+				break;
+			}
+		}
+
+	}
+
+}
